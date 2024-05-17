4f11214f595c30f18e21d37c46f6c9cbbc00423d
Anthony <tony.schomer@gmail.com>
2024-05-16 22:37:13 -0500

    stream update

--- process_streaming_anthony.py
+++ process_streaming_anthony.py
@@ -1,34 +1,27 @@
 import csv
-import socket
+import random
 import time
 import logging
 
 logging.basicConfig(filename="out9.txt", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-HOST = "localhost"
-PORT = 9999
-ADDRESS = (HOST, PORT)
-INPUT_FILE = "people.csv"  # Change the input file name
+INPUT_FILE = "people.csv"
 
-def prepare_message(row):
-    name, age, city = row[0], row[1], row[2]
-    return f"[{name}, {age}, {city}]".encode()
-
-def stream_data(input_file, address):
+def stream_data(input_file):
     with open(input_file, "r") as file:
         reader = csv.reader(file)
         next(reader)  # Skip header
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
         for row in reader:
-            message = prepare_message(row)
-            sock.sendto(message, address)
-            logging.info(f"Sent: {message} to {address}. Hit CTRL-c to stop.")
-            time.sleep(3)
+            name, age, city = row  # Unpack the row tuple
+            message = f"[{name}, {age}, {city}]"
+            logging.info(message)
+            time.sleep(random.randint(1, 3))  # Generate one record every 1-3 seconds
 
 if __name__ == "__main__":
     try:
-        logging.info("Starting fake streaming process.")
-        stream_data(INPUT_FILE, ADDRESS)
+        logging.info("Starting streaming process.")
+        stream_data(INPUT_FILE)
         logging.info("Streaming complete!")
     except Exception as e:
         logging.error(f"An error occurred: {e}")
